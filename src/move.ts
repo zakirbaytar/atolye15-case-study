ba0fff0054b48ae60677d8f7c58f7d3280a78629
Zakir Baytar <zakirrbaytar@gmail.com>
2022-02-18 17:37:46 +0300

    Added type definitions

--- src/move.ts
+++ src/move.ts
@@ -1,5 +1,15 @@
-// Please update this type as same as with the data shape.
-type List = unknown;
+type CustomFile = {
+  id: string;
+  name: string;
+};
+
+type Folder = {
+  id: string;
+  name: string;
+  files: CustomFile[];
+};
+
+type List = Folder[];
 
 export default function move(list: List, source: string, destination: string): List {
   throw new Error('Not implemented');
